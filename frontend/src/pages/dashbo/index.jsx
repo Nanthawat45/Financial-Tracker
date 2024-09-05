a2819fb99640393467ad7fd6f68dc6589b213bb8
Nanthawat45 <Nanthawat.ohm45@gmail.com>
2024-09-05 17:44:27 +0700

    Bug-frontend

--- frontend/src/pages/dashbo/index.jsx
+++ frontend/src/pages/dashbo/index.jsx
@@ -11,7 +11,7 @@ const Dashbard = () => {
   return (
     <div className='max-w-screen-2xl container mx-auto xl:px-24 px-4'>
         <div className='text-center text-3xl md:text-4xl md:leading-snug font-bold my-2'>
-            Welcome {user?}
+           {/* Welcome {user?} */}
         </div>
     </div>
   )
