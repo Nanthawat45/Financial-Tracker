9019e7f52107f9ec445f42cf99c48a01300e44e9
Nanthawat45 <111556174+Nanthawat45@users.noreply.github.com>
2024-10-06 22:33:55 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 const cors = require("cors");
 const corsOptions = {
-    origin:"http://localhost:5173"
+    origin:"https://financial-tracker-inky-psi.vercel.app"
 };
 
 //use Middleware
